7e19af240409e936038130a854b04ca4438425d1
jaikrishann <jaikrishan2001@gmail.com>
2025-04-17 11:21:58 +0530

    new

---
+++ 1 MT 4TH/python/basic.py
@@ -0,0 +1,35 @@
+#print("hello world")
+
+# a = 10 
+# print(type(a))                      #
+
+
+
+
+##String 
+# a = "jaij
+# lkdds"
+# a = """
+# jhjsdjkskjad
+# jhsdajhsad
+# ksadjsa
+# jsad"""                #multi line string
+# print(type(a))
+# print(a)
+
+# print(company.upper())
+# print(company.lower())
+
+# print(company.strip())
+# print(company.lstrip())
+# print(company.rstrip())
+
+# print(company.title())
+# print(company.replace("upflairs","bye"))
+
+company = "upflairs company"
+# print(company.removeprefix("upflairs"))
+# print(company.removesuffix("company"))
+# print(company.count("y"))
+## slicing   [start : stop : skip ]
+print(company[2:8:2])
---
+++ 1 MT 4TH/python/lst.py
@@ -0,0 +1,23 @@
+# lst = ["hello",22.34,22,1,True]
+# print(lst)
+# print(type(lst))
+# print(len(lst))
+
+# print(lst.count("red"))
+# lst.extend(["green","yellow"])
+# lst.index("red")
+# lst.reverse()
+# lst.clear()
+# lst.sort(reverse=True)
+# print(lst[::-1])
+# lst.append("green")
+# lst = ["red","black","blue","red"]
+# lst.insert(1,"hello")
+# lst.pop(1)
+# lst.remove("red")
+# lst1=lst.copy()
+# print(lst1)
+
+
+def addd(a,b):
+    return a+b 
